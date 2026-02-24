037d91b5792f7c3d88b0f8653e49af1b5bbf8fff
nishan.paul.hello@gmail.com <nishan.paul.hello@gmail.com>
2026-02-24 12:14:27 +0600

    refactor: remove skeleton loading UI and return null in ProtectedRoute component

--- admin-dashboard/src/components/ProtectedRoute.tsx
+++ admin-dashboard/src/components/ProtectedRoute.tsx
@@ -3,7 +3,6 @@
 import { useEffect } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import { useAuthStore } from '@/stores/authStore';
-import { Skeleton } from '@/components/ui/skeleton';
 
 import { useUIStore } from '@/stores/uiStore';
 
@@ -36,15 +35,7 @@ export function ProtectedRoute({ children }: ProtectedRouteProps) {
   }, [isAuthenticated, isAdmin, isLoading, openLoginModal, router, pathname]);
 
   if (isLoading || !isAuthenticated || !isAdmin) {
-    return (
-      <div className="flex flex-col space-y-3 p-8">
-        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
-      </div>
-    );
+    return null;
   }
 
   return <>{children}</>;
